cd834de247f790915cc75787d6e1e422cf99e7a7
John Griffiths <indiehead@gmail.com>
2009-05-10 22:03:48 -0400

    init

---
+++ init.rb
@@ -0,0 +1,11 @@
+#toolkit
+require 'rdiscount'
+require 'redcloth'
+
+#pingify
+require 'net/http'
+require 'uri'
+
+#load
+require 'hatchet_harry'
+
---
+++ tasks/pingify_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :pingify do
+#   # Task goes here
+# end
